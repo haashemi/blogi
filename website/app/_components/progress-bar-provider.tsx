3a5cbeae07694c123c499673337c274fd4e222df
Ali Hashemi <mr.ali.haashemi@gmail.com>
2024-10-22 03:08:53 +0330

    chore: remove nprogress

--- website/app/_components/progress-bar-provider.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
-
-export const NProgressBar = () => {
-  return <ProgressBar shallowRouting height="2px" color="#000" options={{ showSpinner: false }} />;
-};
