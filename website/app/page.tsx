f13b67a78bb7e290cd3e305f051173af9e45aecd
Ali Hashemi <mr.ali.haashemi@gmail.com>
2024-10-22 22:23:32 +0330

    feat: add search input and more posts button

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,25 +1,29 @@
 import Link from "next/link";
+import { RiSearch2Line } from "react-icons/ri";
 
 import { blogs } from "./_api/blogs";
 import { BlogCard } from "./_components/blog-card";
+import { Input } from "./_components/ui/input";
 
 export default async function Home() {
-  const data = await blogs("1");
+  const data = await blogs();
 
   return (
     <main className="flex flex-1 flex-col gap-10 px-3">
       <div className="flex flex-col items-center gap-2 pb-6 pt-16">
-        <h1 className="text-6xl font-black">بلاگی</h1>
         <p className="w-full min-w-0 max-w-64 text-pretty text-center font-light text-zinc-500">
-          جایی برای نوشتن در مورد هر چیزی که به ذهنت خلاق نویسنده‌ها می‌رسه
+          بلاگی جایی برای هرکسیه که دوست داره ایده و دیدگاهش رو به نوشته تبدیل کنه
         </p>
 
-        <Link className="mt-5 bg-black px-4 py-2 text-white" href="/profile">
-          بریم بنویسیم
-        </Link>
+        <form action="/blog" className="flex">
+          <Input className="max-w-64" id="query" name="query" placeholder="جستجو..." />
+          <button className="flex size-9 shrink-0 items-center justify-center bg-black text-white" type="submit">
+            <RiSearch2Line className="size-full p-2" />
+          </button>
+        </form>
       </div>
 
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-3">
         <h3 className="text-center text-lg font-bold">آخرین بلاگ‌ها</h3>
 
         <div className="flex flex-col gap-3">
@@ -33,6 +37,15 @@ export default async function Home() {
               title={v.title}
             />
           ))}
+        </div>
+
+        <div className="flex justify-center">
+          <Link
+            className="border border-zinc-400 px-4 py-2 text-sm transition-colors hover:bg-black hover:text-white focus:border-transparent focus:bg-black focus:text-white"
+            href="/blog"
+          >
+            نمایش تمام مقالات
+          </Link>
         </div>
       </div>
     </main>
