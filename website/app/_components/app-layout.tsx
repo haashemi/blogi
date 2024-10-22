3a416e8367884d45d7b3a2ddbb1f76f4a9e1485c
Ali Hashemi <mr.ali.haashemi@gmail.com>
2024-10-22 21:20:37 +0330

    style: increase the header z-index to 20

--- website/app/_components/app-layout.tsx
+++ website/app/_components/app-layout.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 flex h-16 w-full items-center justify-between border border-t-0 border-zinc-300 bg-zinc-100/75 px-3 backdrop-blur">
+    <header className="sticky top-0 z-20 flex h-16 w-full items-center justify-between border border-t-0 border-zinc-300 bg-zinc-100/75 px-3 backdrop-blur">
       <Link className="text-3xl font-black" href="/">
         بلاگی
       </Link>
